419dad42e3bf81f18b38630b9d1738031652f9f6
Christian <christianmillan179@gmail.com>
2022-09-22 10:07:08 +0200

    Declaracion y Sintaxis de Varibles en JavaScript

---
+++ js/variables.js
@@ -0,0 +1,27 @@
+/* Variables */
+
+/* Esta forma de denominacionde variables -- ya no se utiliza */
+var producto = 'Auriculares Gamer'; /* Inicialización de la variable con asignación de valor */
+
+var disponible; /* Inicialización de la variable sin asignación del valor */
+
+producto = true; /* Reasinación del valor de la variable */
+
+disponible = true; /* Asignación del valor a la variable definida */
+
+var producto1 = 'Ordenador',
+    disponible1 = true,         /* Declaración y asignación multiple de variable con su correspondiente valor */
+    categoria = 'Ordenadores';
+
+/* Las asignación de variables no puede comenzar por caracteres numéricos -- ejemplo:   var 1disponible;*/
+
+/* Sin embargo, tu a las variables las puedes caracterizar o asinarles el par de nombre + valor de la siguiente manera - var disponible_; */
+
+/* Estilos para las Variables */
+var nombre_producto = 'Monitor HD'  /* Variable con la denominación del tipo "underscore" */
+var nombreProducto = 'Monitor HD'   /* Variable con la denominación del tipo "camelcase" - + Utilizada */
+var NombreProducto = 'Monitor HD'   /* Variable con la denominación del tipo "pascalcase" */
+var nombreproducto = 'Monitor HD'   /* Variable con la denominación del tipo "lowercase" */
+
+/* Visulación del código en la consola del navegador con distinción de los nombres de la variables */
+console.log(producto); /* console.log(PRODUCTO) - ¡ERROR EN LA CONSOLA! */
