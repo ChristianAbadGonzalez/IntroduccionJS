1db593c904ec0a268de87755b3e22b597e022118
Christian <christianmillan179@gmail.com>
2022-09-29 11:06:54 +0200

    Metodos de String en JavaScript

--- js/datos.js
+++ js/datos.js
@@ -1,5 +1,8 @@
 /* CADENAS DEL TIPO DE DATOS STRING */
-const producto = "Monitor de 20 pulgadas\""; /* Manera de asignar un tamaño a una variable */
+const producto = 'Monitor de 20 pulgadas\"'; /* Manera de asignar un tamaño a una variable */
+const tweet = 'Estamos delante de un programa de aprendizaje de JavaScript de los tipos de variables de datos';
+const producto2 = 'Monitor de 30 pulgadas\"'; /* Manera de asignar un tamaño a una variable */
+const email = 'correo@correo.com'
 
 /* Otras formas de arquitectura de asignación del tipo de datos String */
 
@@ -7,3 +10,16 @@ const producto = "Monitor de 20 pulgadas\""; /* Manera de asignar un tamaño a u
 /*const producto3 = new String('Monitor de 40 pulgadas');*/
 
 console.log(producto);
+/* console.log(producto.length);*/ /* Propiedad para saber cuantos caracteres hemos escrito en la cadena de texto */
+console.log(producto2); 
+
+/* IndexOf */ /* Manera de saber en que psoición se encuentra la palabra que necesitamos encontrar en la cadena de caracteres */
+/* Si en dicha extensión al visulizarlo por la consola nos sale un numero negativo como este "-1", eso nos quiere decir
+   que dicha palabra en el formato o constate declarada no lo ha encontrado o bien porque no se ha declarado dicho string en la variable en cuestión */
+console.log(tweet.indexOf('Javascript'));
+
+/* Includes -- nos devuelve si dicha variable es "true" / "false" */
+console.log(tweet.includes('Javascript'));
+
+/* Manera de verificar si el correo es valido o no mediante filtración de los valores de la constante */
+console.log(email.indexOf(email));
