26efe809d45e05344a84305c7211d227b197657c
Christian <christianmillan179@gmail.com>
2022-09-29 10:48:58 +0200

    Gest-asignación de String en nuestros programas

---
+++ js/datos.js
@@ -0,0 +1,9 @@
+/* CADENAS DEL TIPO DE DATOS STRING */
+const producto = "Monitor de 20 pulgadas\""; /* Manera de asignar un tamaño a una variable */
+
+/* Otras formas de arquitectura de asignación del tipo de datos String */
+
+/* const producto2 = String('Monitor de 30 pulgadas');*/
+/*const producto3 = new String('Monitor de 40 pulgadas');*/
+
+console.log(producto);
